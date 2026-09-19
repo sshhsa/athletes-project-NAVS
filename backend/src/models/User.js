76d281552ef002f4126235462898f4f5f52796c0
Alex Marynkevych <alexandr.mr253@gmail.com>
2026-09-20 01:31:12 +0300

    feat: add User model with password hashing and duplicate email handling

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,1 +1,57 @@
-// TODO: mongoose-модель User (email, password, verify, avatarURL, token)
+import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
+
+const emailRegexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+    },
+    avatarURL: {
+      type: String,
+      default: null,
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.post("save", (error, doc, next) => {
+  if (error.name === "MongoServerError" && error.code === 11000) {
+    next(new Error("Email in use"));
+  } else {
+    next(error);
+  }
+});
+
+export const User = model("User", userSchema);
