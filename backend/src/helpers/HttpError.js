1519b8093b7a5741ab44ef5610414ffe08e1ebad
Alex Marynkevych <alexandr.mr253@gmail.com>
2026-09-19 22:54:41 +0300

    feat(backend): add HttpError factory helper

--- backend/src/helpers/HttpError.js
+++ backend/src/helpers/HttpError.js
@@ -1,1 +1,13 @@
-// TODO: фабрика http-помилок (обгортка над http-errors)
+import createError from "http-errors";
+
+const errorMessages = {
+  400: "Bad Request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not Found",
+  409: "Conflict",
+};
+
+export const HttpError = (status, message = errorMessages[status]) => {
+  return createError(status, message);
+};
