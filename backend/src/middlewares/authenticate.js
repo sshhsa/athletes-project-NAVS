708805be3cacd14a494e0661f40ae363291a4e92
Alex Marynkevych <alexandr.mr253@gmail.com>
2026-09-20 01:50:17 +0300

    feat: add authServices with register, login, logout logic

--- backend/src/middlewares/authenticate.js
+++ backend/src/middlewares/authenticate.js
@@ -1,1 +1,27 @@
-// TODO: перевірка JWT з заголовка Authorization
+import jwt from "jsonwebtoken";
+
+import { HttpError } from "../helpers/HttpError.js";
+import { User } from "../models/User.js";
+
+export const authenticate = async (req, res, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+
+  if (bearer !== "Bearer" || !token) {
+    return next(HttpError(401, "Not authorized"));
+  }
+
+  try {
+    const { id } = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(id);
+
+    if (!user || !user.token || user.token !== token) {
+      return next(HttpError(401, "Not authorized"));
+    }
+
+    req.user = user;
+    next();
+  } catch {
+    next(HttpError(401, "Not authorized"));
+  }
+};
--- backend/src/services/authServices.js
+++ backend/src/services/authServices.js
@@ -1,1 +1,66 @@
-// TODO: бізнес-логіка авторизації
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { randomUUID } from "node:crypto";
+
+import { User } from "../models/User.js";
+import { HttpError } from "../helpers/HttpError.js";
+import { sendVerificationEmail } from "./sendEmail.js";
+
+export const registerUser = async ({ name, email, password }) => {
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    throw HttpError(409, "Email in use");
+  }
+
+  const verificationToken = randomUUID();
+
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    verificationToken,
+  });
+
+  await sendVerificationEmail({ email, verificationToken });
+
+  return newUser;
+};
+
+export const loginUser = async ({ email, password }) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  if (!user.verify) {
+    throw HttpError(401, "Email is not verified");
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordCorrect) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "24h",
+  });
+
+  user.token = token;
+  await user.save();
+
+  return { token, user };
+};
+
+export const logoutUser = async (userId) => {
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw HttpError(401, "Not authorized");
+  }
+
+  user.token = null;
+  await user.save();
+};
