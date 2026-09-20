153c5cff03ab40efdd01ddc3c947c865a7241f15
Alex Marynkevych <alexandr.mr253@gmail.com>
2026-09-20 13:06:39 +0300

    feat: add Athlete model with owner reference

--- backend/src/models/Athlete.js
+++ backend/src/models/Athlete.js
@@ -1,2 +1,34 @@
-// TODO: mongoose-модель Athlete
-// поля за даними з макету: photo, fullName, department, course, sport, rank, achievements[]
+import { Schema, model } from "mongoose";
+
+const athleteSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    sport: {
+      type: String,
+      required: [true, "Sport is required"],
+    },
+    country: {
+      type: String,
+      required: [true, "Country is required"],
+    },
+    dateOfBirth: {
+      type: Date,
+      required: [true, "Date of birth is required"],
+    },
+    avatarURL: {
+      type: String,
+      default: null,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
+
+export const Athlete = model("Athlete", athleteSchema);
